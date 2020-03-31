cdbd274d6ed2230bf81396ad1448b355cb67c1ec
Semergal <37205006+Semergal@users.noreply.github.com>
2020-03-31 13:47:40 +1000

    new

---
+++ js/script-a61.js
@@ -0,0 +1,38 @@
+﻿var value = 0;
+
+function changeProgressBar(count) {
+
+    if (value == 100) {
+        value = 0;
+    }
+
+    value += count;
+
+    if (value > 100) {
+        value = 100;
+    }
+
+    $("#progress-bar").width(`${value}%`);
+    $("#progress-bar").text(`${value}%`);
+
+}
+
+function init() {
+
+    changeProgressBar(value)
+
+    $("#btn-1").click(function () {
+        changeProgressBar(1);
+    });
+
+    $("#btn-3").click(function () {
+        changeProgressBar(3);
+    });
+
+    $("#btn-7").click(function () {
+        changeProgressBar(7);
+    });
+
+}
+
+$(document).ready(init);
